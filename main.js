cfe75c72ce486a6a09d3450cff34313da14d4559
Mres <tu8fme6jw@code.edu.az>
2024-04-02 13:20:07 +0400

    taskclass

--- main.js
+++ main.js
@@ -7,23 +7,28 @@
 //         console.log(`Ad:${this.ad},Soyad: ${this.soyad},Yas: ${this.yas},Cins : ${this.cins}`)
 //     }
 // }
-// var insan1 =new Human ("John","Doe",32,"kisi");
-// insan1.melumat(); // Ad:John,Soyad:Doe,Yas:32,Cins:kisi
+// var insan1 =new Human ("John","Doe",21,"kisi");
+// insan1.melumat(); // Ad:John,Soyad:Doe,Yas:21,Cins:kisi
 
 
-// function Human(ad, soyad) {
-//     this.ad = ad;
-//     this.soyad = soyad;
-    
-//     this.getFullName = function() {
-//         return this.soyad[0]+"."+this.ad;
-//     }
-// }
-// var ad = prompt("Adınızı daxil edin:");
-// var soyad = prompt("Soyadinizi daxil edin");
-// var insan = new Human(ad, soyad);
-// var tamAd = insan.getFullName();
-// console.log(tamAd);
+
+
+function Human(ad, soyad) {
+    this.ad = ad;
+    this.soyad = soyad; 
+    this.getFullName = function() {
+        return this.soyad[0] + "." + this.ad;
+    }
+}
+
+var ad = prompt("Adınızı daxil edin:");
+var soyad = prompt("Soyadınızı daxil edin:");
+var insan = new Human(ad, soyad);
+var tamAd = insan.getFullName().toUpperCase();
+console.log(tamAd);
+
+
+
 
 
 // function Employee(department,experienceYear,salary =0) {
@@ -33,6 +38,7 @@
 // }
 // let isci1 = prompt("department,experience year,salary daxil edin:");
 // console.log(isci1);
+
 
 
 // function Employee(department, experienceYear, salary = 0) {
@@ -46,8 +52,35 @@
 //         return id;
 //     }
 // }
-// let isci1 = prompt("department,experience year,salary daxil edin:");
-// console.log(isci1);
+// let employee1 = new Employee("Software", 5, 5000);
+// console.log(employee1.generateID());
+// let employee2 = new Employee("Marketing", 3, 4500);
+// console.log(employee2.generateID());
+
+
+
+
+
+
+// function Employee(department, experienceYear, salary = 0) {
+//     this.department = department;
+//     this.experienceYear = experienceYear;
+//     this.salary = salary;
+// }
+// var employees = [
+//     new Employee("IT", 5, 5000),
+//     new Employee("Finance", 7, 6000),
+//     new Employee("Marketing", 4, 4500),
+// ];
+// console.log(employees);
+
+
+
+
+
+
+
+
 
 
 
